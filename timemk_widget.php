cae1bf27450ac09a1d02887058b6b3533c5e1054
Борис Кузманов <boriskuzmanov2@gmail.com>
2013-06-21 17:08:49 +0200

    issues #2 & #3 -> solved
    - Среден проблемот со зачувување на избраните опции
    - Гланцање завршено :)

--- timemk_widget.php
+++ timemk_widget.php
@@ -21,7 +21,7 @@ class TIMEMK_Widget extends WP_Widget {
 	function TIMEMK_Widget() {
 		$widget_ops = array('classname' => 'timemk', 'description' => __('Бидете во тек со најновите наслови на Time.mk.', 'timemk'));	
 		$control_ops = array('width' => 300, 'height' => 350, 'id_base' => 'timemk-widget');
-		$this->WP_Widget('timemk-widget', __('TIME.MK Widget', 'timemk'), $widget_ops, $control_ops);
+		$this->WP_Widget('timemk-widget', __('TIME.MK виџет', 'timemk'), $widget_ops, $control_ops);
 	}
 	
 	function widget($args, $instance) {
@@ -107,7 +107,7 @@ class TIMEMK_Widget extends WP_Widget {
 				$kat = 'kultura';
 				printf('<iframe frameborder="0" seamless src="http://www.time.mk/widget/'.$kat.''.$brVesti.''.$neshto.'" height="'.$visina.'" width="'.$sirina.'"></iframe>');
 				break;
-			case 'Наука / Технологија':
+			case 'Наука/Технологија':
 				$kat = 'tehnologija';
 				printf('<iframe frameborder="0" seamless src="http://www.time.mk/widget/'.$kat.''.$brVesti.''.$neshto.'" height="'.$visina.'" width="'.$sirina.'"></iframe>');
 				break;
@@ -115,11 +115,11 @@ class TIMEMK_Widget extends WP_Widget {
 				$kat = 'scena';
 				printf('<iframe frameborder="0" seamless src="http://www.time.mk/widget/'.$kat.''.$brVesti.''.$neshto.'" height="'.$visina.'" width="'.$sirina.'"></iframe>');
 				break;
-			case 'Занимливости / Забава':
+			case 'Занимливости/Забава':
 				$kat = 'fun';
 				printf('<iframe frameborder="0" seamless src="http://www.time.mk/widget/'.$kat.''.$brVesti.''.$neshto.'" height="'.$visina.'" width="'.$sirina.'"></iframe>');
 				break;
-			case 'Авотомобили / Мотори':
+			case 'Авотомобили/Мотори':
 				$kat = 'avto';
 				printf('<iframe frameborder="0" seamless src="http://www.time.mk/widget/'.$kat.''.$brVesti.''.$neshto.'" height="'.$visina.'" width="'.$sirina.'"></iframe>');
 				break;
@@ -131,7 +131,7 @@ class TIMEMK_Widget extends WP_Widget {
 				$kat = 'film';
 				printf('<iframe frameborder="0" seamless src="http://www.time.mk/widget/'.$kat.''.$brVesti.''.$neshto.'" height="'.$visina.'" width="'.$sirina.'"></iframe>');
 				break;
-			case 'Живот / Уметност':
+			case 'Живот/Уметност':
 				$kat = 'life';
 				printf('<iframe frameborder="0" seamless src="http://www.time.mk/widget/'.$kat.''.$brVesti.''.$neshto.'" height="'.$visina.'" width="'.$sirina.'"></iframe>');
 				break;
@@ -143,11 +143,11 @@ class TIMEMK_Widget extends WP_Widget {
 				$kat = 'zdravje';
 				printf('<iframe frameborder="0" seamless src="http://www.time.mk/widget/'.$kat.''.$brVesti.''.$neshto.'" height="'.$visina.'" width="'.$sirina.'"></iframe>');
 				break;
-			case 'Храна / Фитнес':
+			case 'Храна/Фитнес':
 				$kat = 'hrana';
 				printf('<iframe frameborder="0" seamless src="http://www.time.mk/widget/'.$kat.''.$brVesti.''.$neshto.'" height="'.$visina.'" width="'.$sirina.'"></iframe>');
 				break;
-			case 'Убавина / Стил / Мода':
+			case 'Убавина/Стил/Мода':
 				$kat = 'style';
 				printf('<iframe frameborder="0" seamless src="http://www.time.mk/widget/'.$kat.''.$brVesti.''.$neshto.'" height="'.$visina.'" width="'.$sirina.'"></iframe>');
 				break;
@@ -189,12 +189,12 @@ class TIMEMK_Widget extends WP_Widget {
 	}
 
 	function update($new_instance, $old_instance) {
-		$instance = $old_instance;
-
-		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance = $new_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
 		$instance['tip'] = strip_tags($new_instance['tip']);
 		$instance['kategorija'] = strip_tags($new_instance['kategorija']);
 		$instance['brVesti'] = strip_tags($new_instance['brVesti']);
+		
 		return $instance;
 	}
 
@@ -210,31 +210,56 @@ class TIMEMK_Widget extends WP_Widget {
 
 		<strong>Тип:</strong>
 		<p>
-		<select name="<?php echo $this->get_field_name('tip'); ?>" id="<?php echo $this->get_field_id('tip'); ?>" class="widefat"> 
-			<?php $options = array('Вертикален', 'Хоризонтален'); foreach ($options as $option) { 
-				echo '<option value="' . $option . '" id="' . $option . '"', $tip == $option ? ' selected="selected"' : '', '>', $option, '</option>'; 
-			} ?> 
+		<select name="<?php echo $this->get_field_name('tip'); ?>" id="<?php echo $this->get_field_id('tip'); ?>" class="widefat" id="tipWidget"> 
+			<option <?php selected('Вертикален', $instance['kategorija'], true); ?> value="Вертикален">Вертикален</option>
+			<option <?php selected('Хоризонтален', $instance['kategorija'], true); ?> value="Хоризонтален">Хоризонтален</option>
 		</select>
 		</p>
 		
 		<strong>Категорија:</strong>
 		<p>
 		<select name="<?php echo $this->get_field_name('kategorija'); ?>" id="<?php echo $this->get_field_id('kategorija'); ?>" class="widefat"> 
-			<?php $options = array('ВЕСТИ', 'ЖИВОТ', 'СПОРТ', 'Македонија','Економија','Балкан','Свет','Скопје','Црна Хроника','Република','Култура','Наука / Технологија','Сцена','Занимливости / Забава','Автомобили / Мотори','Музика','Филм','Живот / Уметност','Патувања','Здравје','Храна / Фитнес','Убавина / Стил / Мода', 'Интима', 'Фудбал', 'Кошарка', 'Ракомет', 'Автомото', 'Тенис', 'Останато'); foreach ($options as $option) { 
-				echo '<option value="' . $option . '" id="' . $option . '"', $kategorija == $option ? ' selected="selected"' : '', '>', $option, '</option>'; 
-			} ?> 
+			<option <?php selected('ВЕСТИ', $instance['kategorija'], true); ?> value="ВЕСТИ">ВЕСТИ</option>
+			<option <?php selected('ЖИВОТ', $instance['kategorija'], true); ?> value="ЖИВОТ">ЖИВОТ</option>
+			<option <?php selected('СПОРТ', $instance['kategorija'], true); ?> value="СПОРТ">СПОРТ</option>
+			<option <?php selected('Македонија', $instance['kategorija'], true); ?> value="Македонија">Македонија</option>
+			<option <?php selected('Економија', $instance['kategorija'], true); ?> value="Економија">Економија</option>
+			<option <?php selected('Балкан', $instance['kategorija'], true); ?> value="Балкан">Балкан</option>
+			<option <?php selected('Свет', $instance['kategorija'], true); ?> value="Свет">Свет</option>
+			<option <?php selected('Скопје', $instance['kategorija'], true); ?> value="Скопје">Скопје</option>
+			<option <?php selected('Црна Хроника', $instance['kategorija'], true); ?> value="Црна Хроника">Црна Хроника</option>
+			<option <?php selected('Република', $instance['kategorija'], true); ?> value="Република">Република</option>
+			<option <?php selected('Култура', $instance['kategorija'], true); ?> value="Култура">Култура</option>
+			<option <?php selected('Наука/Технологија', $instance['kategorija'], true); ?> value="Наука/Технологија">Наука/Технологија</option>
+			<option <?php selected('Сцена', $instance['kategorija'], true); ?> value="Сцена">Сцена</option>
+			<option <?php selected('Занимливости/Забава', $instance['kategorija'], true); ?> value="Занимливости/Забава">Занимливости/Забава</option>
+			<option <?php selected('Автомобили/Мотори', $instance['kategorija'], true); ?> value="Автомобили/Мотори">Автомобили/Мотори</option>
+			<option <?php selected('Музика', $instance['kategorija'], true); ?> value="Музика">Музика</option>
+			<option <?php selected('Филм', $instance['kategorija'], true); ?> value="Филм">Филм</option>
+			<option <?php selected('Живот/Уметност', $instance['kategorija'], true); ?> value="Живот/Уметност">Живот/Уметност</option>
+			<option <?php selected('Патување', $instance['kategorija'], true); ?> value="Патување">Патување</option>
+			<option <?php selected('Здравје', $instance['kategorija'], true); ?> value="Здравје">Здравје</option>
+			<option <?php selected('Храна/Фитнес', $instance['kategorija'], true); ?> value="Храна/Фитнес">Храна/Фитнес</option>
+			<option <?php selected('Убавина/Стил/Мода', $instance['kategorija'], true); ?> value="Убавина/Стил/Мода">Убавина/Стил/Мода</option>
+			<option <?php selected('Интима', $instance['kategorija'], true); ?> value="Интима">Интима</option>
+			<option <?php selected('Фудбал', $instance['kategorija'], true); ?> value="Фудбал">Фудбал</option>
+			<option <?php selected('Кошарка', $instance['kategorija'], true); ?> value="Кошарка">Кошарка</option>
+			<option <?php selected('Ракомет', $instance['kategorija'], true); ?> value="Ракомет">Ракомет</option>
+			<option <?php selected('Автомото', $instance['kategorija'], true); ?> value="Автомото">Автомото</option>
+			<option <?php selected('Тенис', $instance['kategorija'], true); ?> value="Тенис">Тенис</option>
+			<option <?php selected('Останато', $instance['kategorija'], true); ?> value="Останато">Останато</option>
 		</select>	
 		</p>
 
 		<strong>Број на вести</strong>
 		<p>
-		<select name="<?php echo $this->get_field_name('brVesti'); ?>" id="<?php echo $this->get_field_id('brVesti'); ?>" class="widefat"> 
-			<?php $options = array('3', '4', '5', '8'); foreach ($options as $option) { 
-				echo '<option value="' . $option . '" id="' . $option . '"', $brVesti == $option ? ' selected="selected"' : '', '>', $option, '</option>'; 
-			} ?> 
+		<select name="<?php echo $this->get_field_name('brVesti'); ?>" value="<?php echo $instance['brVesti']; ?>" class="widefat"> 
+			<option <?php selected('3', $instance['brVesti'], true); ?> value="3">3</option>
+			<option <?php selected('4', $instance['brVesti'], true); ?> value="4">4</option>
+			<option <?php selected('5', $instance['brVesti'], true); ?> value="5">5</option>
+			<option <?php selected('8', $instance['brVesti'], true); ?> value="8">8</option>
 		</select>
 		</p>
-
 	<?php
 	}
 }
